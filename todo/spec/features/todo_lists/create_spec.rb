61e476531bca87270951d3998afffd0a04389edc
Ben Bledsoe <bledsoedesigns@gmail.com>
2016-04-16 15:17:31 -0500

    Adding example test

---
+++ todo/spec/features/todo_lists/create_spec.rb
@@ -0,0 +1,9 @@
+require_relative '../../spec_helper'
+
+describe "Creating todo lists" do
+  it "redirects to the todo lists index page on success" do
+    # visit "/todo_lists"
+    # click_link "New Todo list"
+    # expect(page).to have_content("New Todo List")
+  end
+end
