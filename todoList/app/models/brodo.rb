1dfb333d2793b9a62b0dc3ee415c626b015cb445
Ben Bledsoe <bledsoedesigns@gmail.com>
2016-04-18 13:23:36 -0500

    Added validations for a todo description

--- todoList/app/models/brodo.rb
+++ todoList/app/models/brodo.rb
@@ -1,4 +1,7 @@
 class Brodo < ActiveRecord::Base
   validates :title, presence: true
   validates :title, length: {minimum: 3}
+
+  validates :description, presence: true
+  validates :description, length: {minimum: 5}
 end
